588afd42cf7681c6267c3ece48646ed6461571ed
Dan Griffin <dan-griffin@live.com>
2017-12-04 16:32:41 -0500

    Small fixes - doesnt compile

--- src/motor_wifi.c
+++ src/motor_wifi.c
@@ -2,7 +2,7 @@
 #include <task.h>
 #include <platform_stdlib.h>
 #include <semphr.h>
-#include "sensor_wifi.h"
+#include "motor_wifi.h"
 #include "wifi_constants.h"
 #include "wifi_structures.h"
 #include "lwip_netconf.h"
@@ -15,7 +15,16 @@
 #include "device.h"
 #include "gpio_api.h"
 
-#define GPIO_LED_PIN       PC_3
+#define GPIO_MOTOR_UP       PC_1
+#define GPIO_MOTOR_DOWN       PC_3
+#define GPIO_LIMIT PC_4
+
+#define SERVER_PORT     80
+#define LISTEN_QLEN     2
+
+static gpio_t gpio_motor_up;
+static gpio_t gpio_motor_down;
+static gpio_t gpio_limit;
 
 static SemaphoreHandle_t wifiBufSem = 0;
 static char wifiBuf[50];
@@ -23,6 +32,9 @@ static char wifiBufSendFlag;
 
 char* ssid;
 char* password;
+
+
+static int tx_exit = 0, rx_exit = 0;
 
 static void wifi_socket_thread(void *param)
 {
@@ -37,6 +49,23 @@ static void wifi_socket_thread(void *param)
 
 void start_sensor_wifi()
 {
+	gpio_init(&gpio_motor_up, GPIO_MOTOR_UP);
+	gpio_dir(&gpio_motor_up, PIN_OUTPUT);    // Direction: Output
+	gpio_mode(&gpio_motor_up, PullNone);     // No pull
+	gpio_write(&gpio_motor_up, 0);
+
+	gpio_init(&gpio_motor_down, GPIO_MOTOR_DOWN);
+	gpio_dir(&gpio_motor_down, PIN_OUTPUT);     // Direction: Output
+	gpio_mode(&gpio_motor_down, PullNone);       // No pull
+	gpio_write(&gpio_motor_down, 0);
+
+	gpio_init(&gpio_limit, GPIO_LIMIT);
+	gpio_dir(&gpio_limit, PIN_INPUT);     // Direction: Input
+	gpio_mode(&gpio_limit, PullNone);       // External Pull Down
+
+
+	vTaskDelay(1000);
+
 	connect_to_network();
 	wifiBufSem = xSemaphoreCreateMutex(); // initialize semaphore
 	if(xTaskCreate(wifi_socket_thread, ((const char*)"WiFi Socket Thread"), 2048, NULL, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
@@ -46,21 +75,15 @@ void start_sensor_wifi()
 
 int connect_to_network()
 {
-	gpio_t gpio_led;
-	gpio_init(&gpio_led, GPIO_LED_PIN);
-	gpio_dir(&gpio_led, PIN_OUTPUT);    // Direction: Output
-	gpio_mode(&gpio_led, PullNone);     // No pull
-	gpio_write(&gpio_led, 0);
 	wifi_on(RTW_MODE_STA); //Resets wifi so we dont get errors
 	wifi_off(); //Resets wifi to avoid errors due to warm start
 	if(wifi_on(RTW_MODE_STA) < 0){
 		printf("\n\r[WIFI] ERROR: wifi_on failed\n");
 		return 0;
 	}
-	vTaskDelay(5000);
 	// Set the auto-reconnect mode with retry 1 time(limit is 2) and timeout 5 seconds.
 	// This command need to be set before invoke wifi_connect() to make reconnection work.
-	wifi_config_autoreconnect(1, 2, 5);
+	wifi_config_autoreconnect(1, 525600, 60);
 	// Connect to AP with PSK-WPA2-AES.
 	ssid = "WLAN_Test";
 	password = "testtest";
@@ -68,12 +91,10 @@ int connect_to_network()
 	{
 		printf("WiFi Connected!");
 		LwIP_DHCP(0, DHCP_START);
-		gpio_write(&gpio_led, 1);
 		vTaskDelay(2000);
 	}
 	else {
 		printf("Error connecting to WIFI!");
-		gpio_write(&gpio_led, 0);
 		return 0;
 	}
 	return 1;
@@ -94,10 +115,6 @@ BaseType_t write_wifi_buffer(const char * dataStart){
 }
 
 
-#define SERVER_PORT     80
-#define LISTEN_QLEN     2
-
-static int tx_exit = 0;
 //static _Sema tcp_tx_rx_sema;
 static _sema tcp_tx_rx_sema;
 
@@ -133,7 +150,54 @@ exit:
 	vTaskDelete(NULL);
 }
 
+static void rx_thread(void *param)
+{
+	int client_fd = * (int *) param;
+	unsigned char buffer[RBUFSIZE] = {'\0'};
+	printf("\n%s start\n", __FUNCTION__);
+	gpio_write(&gpio_relay, 1);
+	while(1) {
+		int ret = 0, sock_err = 0;
+		size_t err_len = sizeof(sock_err);
+		swState[1] = gpio_read(&gpio_switch);
+		rtw_down_sema(&tcp_tx_rx_sema);
+		ret = recv(client_fd, buffer, 16, MSG_DONTWAIT);
+		getsockopt(client_fd, SOL_SOCKET, SO_ERROR, &sock_err, &err_len);
+		rtw_up_sema(&tcp_tx_rx_sema);
+		if (swState[1] != gpio_read(&gpio_switch))
+		{
+			swState[1] ^= 1; // toggle state
+			swState[0] ^= 1;
+			gpio_write(&gpio_relay, swState[1]);
+		}
+		if (buffer[0] == 'o')
+		{
+			swState[0] = 1;
+			gpio_write(&gpio_relay, swState[0]);
+		}
+		else if (buffer[0] == 'f')
+		{
+			swState[0] = 0;
+			gpio_write(&gpio_relay, swState[0]);
+		}
+		if (buffer[0])
+		{
+			printf("Received: %s\n", buffer);
+			memset(&buffer[0], 0, RBUFSIZE);
+		}
+		vTaskDelay(1000);
+		// ret == -1 and socket error == EAGAIN when no data received for nonblocking
+		if((ret == -1) && (sock_err == EAGAIN))
+			continue;
+		else if(ret <= 0)
+			goto exit;
+	}
 
+exit:
+	printf("\n%s exit\n", __FUNCTION__);
+	rx_exit = 1;
+	vTaskDelete(NULL);
+}
 
 static void example_socket_tcp_trx_thread(void *param)
 {
@@ -166,7 +230,6 @@ static void example_socket_tcp_trx_thread(void *param)
 		if(client_fd >= 0) {
 			printf("EXAMPLE SOCKET OUTER LOOP");
 			tx_exit = 1;
-			//RtlInitSema(&tcp_tx_rx_sema, 1);
 			rtw_init_sema(&tcp_tx_rx_sema, 1);
 
 			if(xTaskCreate(tx_thread, ((const char*)"tx_thread"), 512, &client_fd, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
@@ -174,12 +237,14 @@ static void example_socket_tcp_trx_thread(void *param)
 			else
 				tx_exit = 0;
 			vTaskDelay(10);
-
-			//if(xTaskCreate(rx_thread, ((const char*)"rx_thread"), 512, &client_fd, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
-			//	printf("\n\r%s xTaskCreate(rx_thread) failed", __FUNCTION__);
-
+			rx_exit = 1;
+			if(xTaskCreate(rx_thread, ((const char*)"rx_thread"), 512, &client_fd, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
+				printf("\n\r%s xTaskCreate(rx_thread) failed", __FUNCTION__);
+			else
+				rx_exit = 0;
+			vTaskDelay(10);
 			while(1) {
-				if(tx_exit) {
+				if(tx_exit && rx_exit) {
 					close(client_fd);
 					break;
 				}
@@ -187,7 +252,6 @@ static void example_socket_tcp_trx_thread(void *param)
 					vTaskDelay(1000);
 			}
 			printf("\nExample: socket tx/rx place loop\n");
-			//RtlFreeSema(&tcp_tx_rx_sema);
 			rtw_free_sema(&tcp_tx_rx_sema);
 		}
 	}
